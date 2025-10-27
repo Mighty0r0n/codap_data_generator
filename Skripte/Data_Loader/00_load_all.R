763bf3fb4703cba475ab508f8dcb52ba94cc28b1
Daniel Tischler <daniel.tischler1995@googlemail.com>
2025-10-27 13:12:11 +0100

    BUGFIX updated filepaths

--- Skripte/Data_Loader/00_load_all.R
+++ Skripte/Data_Loader/00_load_all.R
@@ -4,9 +4,9 @@
 ############################################################
 
 # ---- 1. ReSurveyGermany Daten laden ----
-source("Skripte/01_load_ReSurveyGermany.R")
+source("Skripte/Data_Loader/01_load_ReSurveyGermany.R")
 
 # ---- 2. DWD Daten laden ----
-# source("Skripte/02_download_dwd_asc_grids.R")
+source("Skripte/Data_Loader/02_load_dwd_asc_grids.R")
 
 message("[DONE] Alle Rohdaten wurden erfolgreich in das Verzeichnis raw_data heruntergeladen.")
