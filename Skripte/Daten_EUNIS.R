49410d8710c6dafe09f6e4a6c5fb4e94c5d8c815
Daniel Tischler <daniel.tischler1995@googlemail.com>
2026-01-12 10:47:26 +0100

    ADDED Soil data

--- Skripte/Daten_EUNIS.R
+++ Skripte/Daten_EUNIS.R
@@ -3,6 +3,7 @@ suppressPackageStartupMessages({
   library(readr)
   library(terra)
   library(fs)
+  library(sf)
   library(tidyr)
   library(stringr)
   library(tibble)
@@ -13,11 +14,56 @@ source("Skripte/utils_data_description.R")
 
 generate_eunis_data <- function(eunis_code_list, write_tmp_file) {
   # Hier wird sich die Grund-Datei geholt
-  survey_df <- merge_re_survey_with_dwd_grids()
+  #survey_df <- merge_re_survey_with_dwd_grids()
   
-  # survey_df <- read_csv("tmp_data/ReSurveyGermany/re_survey_germany_filtered_years.csv")
+  # Debugzeile
+  survey_df <- read_csv("tmp_data/ReSurveyGermany/re_survey_germany_filtered_years.csv")
   
-  # Sind wenige Einträge und enthält wenig zeigbares
+  # Vorbereiten der Bodendaten
+  soil_df <- read_csv2("raw_data/Bodendaten/Bodendaten_Abfrage_UBA.csv") %>%
+    filter(`Bodenmesswert - Oberkante [cm]` > 0,
+           `Bodenmesswert - Unterkante [cm]` <= 30,
+           stringr::str_count(
+             as.character(`Bodenmesswert - Messwert`), ","  # Komische Einträge entfernen
+           ) <= 1
+    ) %>%
+    select(-`Messung - Probenahmedatum`) %>%
+    mutate(
+      `Bodenmesswert - Messwert` = as.numeric(sub(",", ".", `Bodenmesswert - Messwert`)),  # Einfacher Datatype cast
+      `Parameter - Messgröße` = ifelse( # angleichen der pH-Spalte
+        `Parameter - Messgröße` == "pH",
+        "pH-Wert",
+        `Parameter - Messgröße`
+      ),
+      `Parameter - Einheit` = ifelse(
+        `Parameter - Messgröße` == "pH-Wert",
+        "ohne",
+        `Parameter - Einheit`
+      )
+    ) %>%
+    group_by(`Messstellennummer`, `Parameter - Messgröße`) %>% 
+    summarise( # Gruppieren nach Messstellennummer und Messgröße
+      value = median(`Bodenmesswert - Messwert`, na.rm = TRUE),
+      latitude  = first(Latitude),
+      longitude = first(Longitude),
+      n = n(),
+      .groups = "drop"
+    ) %>%
+    pivot_wider( # long to wide formatierung
+      id_cols = c(`Messstellennummer`, latitude, longitude),
+      names_from  = `Parameter - Messgröße`,
+      values_from = value
+    )
+  
+  
+  # Nur ein kleiner check um zu sehen, wieviele Parameter pro Messstelle bemessen worden sind
+  coverage <- soil_df %>%
+    summarise(across(-c(`Messstellennummer`, latitude, longitude),
+                     ~ mean(!is.na(.)))) %>%
+    pivot_longer(everything(), names_to = "messgroesse", values_to = "share_present") %>%
+    arrange(share_present)
+  
+
   survey_df <- survey_df %>% filter(LAYER != 0,
                                     EUNIS %in% eunis_code_list)
   
@@ -164,15 +210,13 @@ generate_eunis_data <- function(eunis_code_list, write_tmp_file) {
     
     # Manche Eunis-codes existieren nicht in ReSurveyGermany. Kleiner Fallback falls weitere eunis codes dazukommen sollten
     if (length(eunis_code_df) == 0) {
-      return(cat("File does not contain any entries"))
+      return(cat("File does not contain any entries for this EUNIS-Code"))
     }
     
 
     scenarios <- c("Presence", "Presence15")#, "presence2", "presence3", "presence4")
     
-    
-    # Hier wird anhand der Presence Spalte dann die Artenzahl pro RS_PLOT berechnet.
-    # Mit n_distinct wird jede Art pro Plot nur einmal gezählt
+
     for (scn in scenarios) {
       new_col <- paste0("Artenzahl_", scn)
       
@@ -203,7 +247,11 @@ generate_eunis_data <- function(eunis_code_list, write_tmp_file) {
     
     # Ich nehme erstmal die ersten paar Einträge jeder Datei
     # Die Zahl ist so gewählt, dass die enddatei etwas knapp unter 5000 Einträgen bleibt.
-    eunis_code_df <- head(eunis_code_df, 1300) %>%
+    
+    
+    #eunis_code_df <- head(eunis_code_df, 1300) %>%
+    
+    eunis_code_df <- eunis_code_df %>%
       mutate(TEMPERATURE15 = TEMPERATURE + 1.5)
     
     combined_eunis_df <- bind_rows(combined_eunis_df, eunis_code_df)
@@ -255,13 +303,105 @@ generate_eunis_data <- function(eunis_code_list, write_tmp_file) {
       Artenzahl_Presence15
     )
   
+
+
+  
+  soil_df <- soil_df %>%
+    filter(!is.na(latitude), !is.na(longitude)) %>%
+    mutate(
+      latitude  = as.numeric(latitude),
+      longitude = as.numeric(longitude),
+      
+      lat_1e6 = latitude / 1e6,
+      lat_1e7 = latitude / 1e7,
+      lat_1e8 = latitude / 1e8,
+      lon_1e6 = longitude / 1e6,
+      lon_1e7 = longitude / 1e7,
+      lon_1e8 = longitude / 1e8,
+      
+      latitude = dplyr::case_when(
+        latitude >= 45 & latitude <= 55 ~ latitude,   # schon Grad
+        lat_1e8  >= 45 & lat_1e8  <= 55 ~ lat_1e8,
+        lat_1e7  >= 45 & lat_1e7  <= 55 ~ lat_1e7,
+        lat_1e6  >= 45 & lat_1e6  <= 55 ~ lat_1e6,
+        TRUE ~ NA_real_
+      ),
+      
+      longitude = dplyr::case_when(
+        longitude >= 5 & longitude <= 16 ~ longitude, # schon Grad
+        lon_1e8   >= 5 & lon_1e8   <= 16 ~ lon_1e8,
+        lon_1e7   >= 5 & lon_1e7   <= 16 ~ lon_1e7,
+        lon_1e6   >= 5 & lon_1e6   <= 16 ~ lon_1e6,
+        TRUE ~ NA_real_
+      )
+    ) %>%
+    select(-lat_1e6, -lat_1e7, -lat_1e8, -lon_1e6, -lon_1e7, -lon_1e8) %>%
+    filter(!is.na(latitude), !is.na(longitude))
+  
+  
+  
+  soil_sf <- soil_df %>%
+    st_as_sf(coords = c("longitude", "latitude"), crs = 4326, remove = FALSE)
+  
+  eunis_sf <- combined_eunis_df %>%
+    st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326, remove = FALSE)
+  
+  
+  soil_m  <- st_transform(soil_sf, 3035)
+  eunis_m <- st_transform(eunis_sf, 3035)
+  
+  nearest_idx <- st_nearest_feature(eunis_m, soil_m)
+  
+  dist_m <- st_distance(eunis_m, soil_m[nearest_idx, ], by_element = TRUE)
+  dist_m <- as.numeric(dist_m)  # units -> numeric
+  
+  
+  soil_attr <- soil_m %>%
+    st_drop_geometry() %>%
+    mutate(soil_row_id = row_number())
+  
+  eunis_out <- eunis_m %>%
+    st_drop_geometry() %>%
+    mutate(
+      soil_row_id = soil_attr$soil_row_id[nearest_idx],
+      Entfernung_Messstelle = dist_m
+    ) %>%
+    left_join(soil_attr, by = "soil_row_id")
+  
+  
+  eunis_out <- eunis_out %>%
+    filter(Entfernung_Messstelle < 10000) %>% # Erstmal filtere ich nach einer entfernung von 10km der Messstelle zur RS_Site, variabel anpassbar
+    select(
+      -soil_row_id,
+      -Messstellennummer,
+      -latitude,
+      -longitude,
+    ) %>%
+    rename(
+      Releve_Nr = RELEVE_NR.x
+    )
+  
   # Speichern des Datensatzes
   result_dir <- path("result_data", "EUNIS")
   dir_create(result_dir, recurse = TRUE)
   result_data_file <- path(result_dir, "combined_eunis.csv")
   
+  # Nur ein kleiner check um zu sehen, wieviele Parameter pro Messstelle bemessen worden sind
+  coverage <- eunis_out %>%
+    summarise(across(-c(`RS_SITE`, LATITUDE, LONGITUDE),
+                     ~ mean(!is.na(.)))) %>%
+    pivot_longer(everything(), names_to = "messgroesse", values_to = "share_present") %>%
+    arrange(share_present)
+  
+  # Für reproduzierbarkeit beim sampeln
+  set.seed(42)
+  
+  # Samplen fürs codap limit
+  eunis_out <- eunis_out[sample(nrow(eunis_out), 4950), ] %>%
+    select(-`Stickstoff gesamt`, -`Phosphor gesamt`) # Bei Messstellenentfernung von unter 10km haben wir keine Messwerte für die Messgrößen
+
   # Dieser Datensatz ist schonmal in CODAP kopierbar.
-  write.csv(combined_eunis_df, file = result_data_file, row.names = FALSE)
+  write.csv(eunis_out, file = result_data_file, row.names = FALSE)
   
   return(combined_eunis_df)
 }
@@ -275,7 +415,7 @@ eunis_code_list = c("T17", "T18", "R22", "V11", "V15")
 
 # write_tmp_file = TRUE wenn einzeldatensätze zu den einzelnen EUNIS Flächen mitgeneriert werden sollen. Diese landen im tmp_data ordner
 tmp_eunis_code_df = generate_eunis_data(
-  eunis_code = eunis_code_list,
+  eunis_code_list = eunis_code_list,
   write_tmp_file = TRUE
   )
 
